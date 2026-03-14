9222cb59c337077d83f8c753a629cc875475f275
mrahms <mridula.rahmsdorf@gmail.com>
2026-03-14 14:34:37 -0700

    Fix npm package name typo

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ BASE_URL=${BASE_URL:-"http://0.0.0.0:$PORT"}
 export APOLLO_API_KEY=${APOLLO_API_KEY}
 
 npx -y supergateway \
-    --stdio "npx -y @thevergegroup/apollo-io-mcp@latest" \
+    --stdio "npx -y @thevgergroup/apollo-io-mcp@latest" \
     --port $PORT \
     --baseUrl $BASE_URL \
     --ssePath /sse \
