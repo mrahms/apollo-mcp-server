360e2fe083c360da1a041abfc3e04ed41182ce61
mrahms <mridula.rahmsdorf@gmail.com>
2026-03-14 14:31:28 -0700

    Change default PORT to 8080 and add CORS option

--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
 
-PORT=${PORT:-8000}
+PORT=${PORT:-8080}
 BASE_URL=${BASE_URL:-"http://0.0.0.0:$PORT"}
 
 export APOLLO_API_KEY=${APOLLO_API_KEY}
 
 npx -y supergateway \
-  --stdio "npx -y @thevgergroup/apollo-io-mcp@latest" \
-  --port $PORT \
-  --baseUrl $BASE_URL \
-  --ssePath /sse \
-  --messagePath /message
+    --stdio "npx -y @thevergegroup/apollo-io-mcp@latest" \
+    --port $PORT \
+    --baseUrl $BASE_URL \
+    --ssePath /sse \
+    --messagePath /message \
+    --cors
